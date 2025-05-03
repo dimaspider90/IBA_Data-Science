65258159b8ec3f2d071e5d594029177331cee913
dimaspider90 <dimaspider1990@gmail.com>
2025-05-03 20:06:12 +0300

    Add files via upload

---
+++ Circles/src/main/java/by/iba/Functions.java
@@ -0,0 +1,22 @@
+package by.iba;
+
+public class Functions {
+    public Functions (){}
+    public void compare(Circle circle1, Circle circle2) {
+        int distance = Math.abs(circle1.getX() - circle2.getX());
+        int radiusSum = circle1.getR() + circle2.getR();
+        int radiusDiff = Math.abs(circle1.getR() - circle2.getR());
+
+        if (distance > radiusSum) {
+            System.out.println("Окружности не пересекаются, расположены отдельно на оси.");
+        } else if (distance == radiusSum) {
+            System.out.println("Окружности соприкасаются в одной точке.");
+        } else if (distance < radiusDiff) {
+            System.out.println("Окружности не пересекаются. Присутствует вхождение.");
+        } else if (distance == radiusDiff) {
+            System.out.println("Окружности соприкасаются в одной точке. Присутствует вхождение.");
+        } else {
+            System.out.println("Окружности пересекаются в двух точках.");
+        }
+    }
+}
